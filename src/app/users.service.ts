dc25b2dcd45cad64d90be8a439c7d2be568f6444
SheilaBirgen <jeronobergen@gmail.com>
2020-01-19 22:33:52 +0300

    add function to users.service.ts

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import{Http, Headers } from '@angular/http';
+import{Http, Headers } from '@angular/common/http';
 import 'rjx/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-private username :string;
+private username :"SheilaBirgen";
 private clientid :"af2302f043d2de7db149";
 private clientsecret: "0b8a1734e15335b7f2e88f2e9ade1e4c48069254"; 
 
@@ -18,7 +18,11 @@ private clientsecret: "0b8a1734e15335b7f2e88f2e9ade1e4c48069254";
   }
   getuserInfor(){
 return this.http.get("http://api.github.com/users/" +this.username +"?client_id=" +this.clientid + "?client_secret=" +this.clientsecret
-  },
-  
-  
-}
+  }
+  getRepoInfo(){
+    return this.http.get(this._URL +this.username + '/repos?').pipe(map(res => res));
+
+  }
+  updateUser(username:string){
+    this.username = username;
+  }
