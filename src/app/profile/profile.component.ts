0d1b50eb0fa1eb2959118eee03614a0b695521a0
SheilaBirgen <jeronobergen@gmail.com>
2020-01-21 08:23:21 +0300

    changed html content

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,52 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import{ ProfileService} from 'src/app/service/profile.service';
-import { Users } from '../users';
-import { Repository } from '../repository';
+// import { Component, OnInit } from '@angular/core';
+// import{ ProfileService} from 'src/app/service/profile.service';
+// import { Users } from '../users';
+// import { Repository } from '../repository';
 
 
 
+
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.css']
+// })
+
+
+// export class ProfileComponent implements OnInit {
+//   searchUsers = true;
+
+//   user:any;
+//   repo: any;
+//   username: any;
+//   service: any;
+
+//   constructor(private profileService: ProfileService) {
+//    }
+//    getProfile() {
+//     this.service.getUsername(this.username);
+
+//     this.service.getUser();
+//     this.user = this.service.user;
+
+//     this.service.getRepos(this.username);
+//     this.repo = this.service.repo;
+//     console.log(this.repo);
+//   }
+//   switchSearch() {
+//     this.searchUsers = !this.searchUsers;
+//   }
+
+//   ngOnInit() {
+//     this.service.getUsers();
+//     this.user = this.service.user;
+
+//     this.service.getRepos(this.username);
+//     this.repo = this.service.repo;
+//     const options = {
+//       strings: ['Enter your username please.'],
+      
+//  };
+//   }
+
+// }
+import { Component, OnInit } from "@angular/core";
+import { ProfileService } from 'src/app/service/profile.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
-
-
 export class ProfileComponent implements OnInit {
-  searchUsers = true;
-
-  user: Users;
-  repo: Repository;
-  username: string;
-  service: any;
-
+  user: any;
+  repos: any;
+  username: any;
   constructor(private profileService: ProfileService) {
-   }
-   getProfile() {
-    this.service.getUsername(this.username);
-
-    this.service.getUser();
-    this.user = this.service.user;
-
-    this.service.getRepos(this.username);
-    this.repo = this.service.repo;
-    console.log(this.repo);
+    this.profileService.getUser().subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+    this.profileService.getRepos().subscribe(repos => {
+      this.repos = repos;
+      console.log(this.repos);
+    });
   }
-  switchSearch() {
-    this.searchUsers = !this.searchUsers;
-  }
+  search() {
+    this.profileService.updateUsername(this.username);
 
-  ngOnInit() {
-    this.service.getUsers();
-    this.user = this.service.user;
-
-    this.service.getRepos(this.username);
-    this.repo = this.service.repo;
-    const options = {
-      strings: ['Enter your username please.'],
-      
- };
+    this.profileService.getUser().subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+    this.profileService.getRepos().subscribe(repos => {
+      this.repos = repos;
+      console.log(this.repos);
+    });
   }
 
+  ngOnInit() {}
 }
--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,77 +1,118 @@
-import { Injectable } from '@angular/core';
-import{HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { Users } from '../users';
-import { Repository } from '../repository';
+// import { Injectable } from '@angular/core';
+// import{HttpClient } from '@angular/common/http';
+// import { environment } from 'src/environments/environment';
+// import { Users } from '../users';
+// import { Repository } from '../repository';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ProfileService {
-  user:Users;
-  repo:Repository;
-  private userName:string;
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProfileService {
+//   user:Users;
+//   repo:Repository;
+//   private userName:string;
   
 
-apiKey:string = environment.apiKey;
-baseUrl: string = environment.baseUrl;
-  getuserName: any;
+// apiKey:string = environment.apiKey;
+// baseUrl: string = environment.baseUrl;
+//   getuserName: any;
  
-  constructor( private http:HttpClient) { 
-    this.user = new Users("","","","",0,"","","",new Date());
-    this.userName = 'SheilaBirgen';
+//   constructor( private http:HttpClient) { 
+//     this.user = new Users("","","","",0,"","","",new Date());
+//     this.userName = 'SheilaBirgen';
+//   }
+//   getUser(){
+//     interface ApiResponse{
+//       login:string;
+//       avatar_url: any;
+//       html_url:string;
+//       bio:string;
+//       repos: number;
+//       followers:number;
+//       following:number;
+//     }
+//     const promise = new Promise(((resolve, reject) => {
+//       this.http.get<ApiResponse>('https://github.com/users/' + this.userName + '?access_token=' + this.apiKey)
+//       .toPromise()
+//       .then(res =>{
+//         this.user.login = res.login;
+//         this.user.avatar_url = res.avatar_url;
+//         this.user.html_url = res.html_url;
+//         this.user.repos = res.repos;
+//         this.user.followers = res.followers;
+//         this.user.following = res.following;
+//       },
+//       error => {
+//           // reject(error);
+//       });
+//     }));
+//     return promise;
+//   }
+
+//   getRepos(username:any) {
+
+//     interface ApiResponse {
+//       name: string;
+//       html_url: string;
+//       description: string;
+//     }
+
+//     const promise = new Promise(((resolve, reject) => {
+//       this.http.get<ApiResponse>('https://api.github.com/users/' + this.userName + '/repos?access_token=' + this.apiKey )
+//         .toPromise()
+//         .then(res => {
+//           this.repo = res;
+//     }, error => {
+
+//       reject(error);
+//     });
+//   }));
+//     return promise;
+//   }
+//   getUsername(username:string){
+//     this.userName = username;
+//   }
+
+
+// }
+
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ProfileService {
+  private username = "SheilaBirgen";
+  private client_id = "6e3851f31ccf37f67ba7";
+  private client_secret = "f0c139288bc0270dc4699745cfce81d719ea0046";
+
+  constructor(private http: HttpClient) {
+    console.log("Github service started");
   }
-  getUser(){
-    interface ApiResponse{
-      login:string;
-      avatar_url: any;
-      html_url:string;
-      bio:string;
-      repos: number;
-      followers:number;
-      following:number;
-    }
-    const promise = new Promise(((resolve, reject) => {
-      this.http.get<ApiResponse>('https://github.com/users/' + this.userName + '?access_token=' + this.apiKey)
-      .toPromise()
-      .then(res =>{
-        this.user.login = res.login;
-        this.user.avatar_url = res.avatar_url;
-        this.user.html_url = res.html_url;
-        this.user.repos = res.repos;
-        this.user.followers = res.followers;
-        this.user.following = res.following;
-      },
-      error => {
-          // reject(error);
-      });
-    }));
-    return promise;
+  getUser() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "?client_id=" +
+        this.client_id +
+        "&client_secret=" +
+        this.client_secret
+    );
   }
-
-  getRepos(username:any) {
-
-    interface ApiResponse {
-      name: string;
-      html_url: string;
-      description: string;
-    }
-
-    const promise = new Promise(((resolve, reject) => {
-      this.http.get<ApiResponse>('https://api.github.com/users/' + this.userName + '/repos?access_token=' + this.apiKey )
-        .toPromise()
-        .then(res => {
-          this.repo = res;
-    }, error => {
-
-      reject(error);
-    });
-  }));
-    return promise;
+  getRepos() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "/repos" +
+        "?client_id=" +
+        this.client_id +
+        "&client_secret=" +
+        this.client_secret
+    );
   }
-  getUsername(username:string){
-    this.userName = username;
+  updateUsername(username: any) {
+    this.username = username;
   }
-
-
 }
